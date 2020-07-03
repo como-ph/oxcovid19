48c032f4f77694f0570c4b47f6b480e862dd92f9
Ernest Guevarra <ernest@guevarra.io>
2020-07-03 09:29:43 +0100

    initial commit

---
+++ R/oxcovid19.R
@@ -0,0 +1,19 @@
+################################################################################
+#
+#'
+#' The OxCOVID19 Project \url{https://covid19.eng.ox.ac.uk} aims to increase
+#' our understanding of the COVID-19 pandemic and elaborate possible strategies
+#' to reduce the impact on the society through the combined power of statistical,
+#' mathematical modelling, and machine learning techniques. The OxCOVID19
+#' Database is a large, single-centre, multimodal relational database consisting
+#' of information (using acknowledged sources) related to COVID-19 pandemic.
+#' This package provides an R-specific interface to the OxCOVID19 Database based
+#' on widely-used data handling and manipulation approaches in R.
+#'
+#' @docType package
+#' @keywords internal
+#' @name oxcovid19
+#'
+#
+################################################################################
+"_PACKAGE"
