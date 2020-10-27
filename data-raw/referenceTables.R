03015790878c01c926a8820046c03dc5c504d5b9
Ernest Guevarra <ernest@guevarra.io>
2020-10-27 06:49:04 +0000

    update datasets

--- data-raw/referenceTables.R
+++ data-raw/referenceTables.R
@@ -5,18 +5,33 @@ library(stringr)
 
 ## Read HTML page for data sources - https://covid19.eng.ox.ac.uk/data_sources.html -
 ## and extract sources tables
-data_sources <- xml2::read_html(x = "https://covid19.eng.ox.ac.uk/data_sources.html") %>%
+#data_sources <- xml2::read_html(x = "https://covid19.eng.ox.ac.uk/data_sources.html") %>%
+#  rvest::html_nodes(css = ".container_col .table") %>%
+#  rvest::html_table(fill = TRUE)
+
+data_sources <- xml2::read_html(x = "https://raw.githubusercontent.com/covid19db/web-page-data/master/html/data_sources.html") %>%
   rvest::html_nodes(css = ".container_col .table") %>%
   rvest::html_table(fill = TRUE)
 
 ## Clean up column in epidemiology sources table with NA
-data_sources[[1]] <- data_sources[[1]] %>%
-  dplyr::select(Country:`Terms of Use`)
+#data_sources[[1]] <- data_sources[[1]] %>%
+#  dplyr::select(Country:`Terms of Use`)
 
 ## Clean up marked UTF8 in sources for Epidemiology
 data_sources[[1]]$Source <- data_sources[[1]]$Source %>%
   stringr::str_replace(pattern = "é", replacement = "e") %>%
-  stringr::str_replace(pattern = "ü", replacement = "u")
+  stringr::str_replace(pattern = "ü", replacement = "u") %>%
+  stringr::str_replace(pattern = "å", replacement = "a") %>%
+  stringr::str_replace(pattern = "ú", replacement = "u")
+
+## Clean up marked UTF8 in Terms of Use for Epidemiology
+data_sources[[1]]$`Terms of Use` <- data_sources[[1]]$`Terms of Use` %>%
+  stringr::str_replace(pattern = "ó", replacement = "o") %>%
+  stringr::str_replace(pattern = "å", replacement = "a")
+
+## Clean up \n
+data_sources[[5]]$`Terms of Use` <- data_sources[[5]]$`Terms of Use` %>%
+  stringr::str_replace(pattern = "\n                    ", replacement = " ")
 
 ## Convert each table in list to tibble
 for(i in seq_len(length(data_sources))) {
@@ -24,8 +39,12 @@ for(i in seq_len(length(data_sources))) {
 }
 
 ## Rename list
-names(data_sources) <- c("epidemiology", "government_response",
-                         "country_statistics", "mobility", "weather")
+names(data_sources) <- c("epidemiology",
+                         "government_response",
+                         "country_statistics",
+                         "mobility",
+                         "weather",
+                         "administrative_division")
 
 ## Save as .rda
 usethis::use_data(data_sources, overwrite = TRUE, compress = "xz")
