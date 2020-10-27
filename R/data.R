f7d6ebf5c718d6ee2e5a19f0a891725e2df04325
Ernest Guevarra <ernest@guevarra.io>
2020-10-27 18:55:50 +0000

    re-format datasets documentation

--- R/data.R
+++ R/data.R
@@ -3,16 +3,16 @@
 #'
 #' Data sources for OxCOVID19 Database
 #'
-#' @format A named list of 5 tibbles of data sources.
-#' \describe{
-#'   \item{\code{epidemiology}}{Tibble of sources for epidemiology table}
-#'   \item{\code{government_response}}{Tibble of sources for government
-#'     response table}
-#'   \item{\code{country_statistics}}{Tibble of sources for country statistics
-#'     table}
-#'   \item{\code{mobility}}{Tibble of sources for mobility table}
-#'   \item{\code{weather}}{Tibble of sources for weather table}
-#' }
+#' @format A named list of 6 tibbles of data sources for **OxCOVID19 Database**
+#'
+#' | **Table name** | **Description** |
+#' | :--- | :--- |
+#' | **epidemiology** | Tibble of sources for epidemiology table |
+#' | **government_response** | Tibble of sources for government response table |
+#' | **country_statistics** | Tibble of sources for country statistics table |
+#' | **mobility** | Tibble of sources for mobility table |
+#' | **weather** | Tibble of sources for weather table |
+#' | **administrative_division** | Tibble of sources for administrative division table |
 #'
 #' @examples
 #' data_sources
@@ -29,19 +29,16 @@
 #'
 #' Data structures for OxCOVID19 Database
 #'
-#' @format A named list of 6 tibbles of data structures.
-#' \describe{
-#'   \item{\code{epidemiology}}{Tibble of description of fields for epidemiology
-#'     table}
-#'   \item{\code{government_response}}{Tibble of description of fields for
-#'     government response table}
-#'   \item{\code{country_statistics}}{Tibble of description for fields for
-#'     country statistics table}
-#'   \item{\code{mobility}}{Tibble of description for fields for mobility table}
-#'   \item{\code{weather}}{Tibble of description for fields for weather table}
-#'   \item{\code{administrative_division}}{Tibble of description for fields for
-#'     administrative division table}
-#' }
+#' @format A named list of 6 tibbles of data structures for **OxCOVID19 Database**
+#'
+#' | **Table name** | **Description** |
+#' | :--- | :--- |
+#' | **epidemiology** | Tibble of description of fields for epidemiology table |
+#' | **government_response** | Tibble of description of fields for government response table |
+#' | **country_statistics** | Tibble of description for fields for country statistics table |
+#' | **mobility** | Tibble of description for fields for mobility table |
+#' | **weather** | Tibble of description for fields for weather table |
+#' | **administrative_division** | Tibble of description for fields for administrative division table |
 #'
 #' @examples
 #' data_structures
